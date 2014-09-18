0f7677ba2cd95f8bc30afd81eabe5b163d565db8
Ahmed Hassan <a.hassan@buismed.de>
2014-09-18 10:05:50 +0200

    smart.json

--- package.js
+++ package.js
@@ -1,8 +1,10 @@
 Package.describe({
-    summary: "uses k-Nearest-Neighbor to classify audio signals"
+    summary: "uses k-Nearest-Neighbor to classify audio signals",
+    version: "0.0.1",
+    git: " \* PRIVATE *\ "
 });
 
-Package.on_use(function(api, where) {
+Package.onUse(function(api, where) {
 
     api.use(['flow-core', 'webaudio-manager', 'audiofeatures']); // private packages look at smart.json
     api.use(['flowkey:math-tools', 'flowkey:functionstack', 'flowkey:dsp', 'flowkey:histogram', 'underscore']); // community packages -> athmosphere
@@ -17,7 +19,7 @@ Package.on_use(function(api, where) {
     api.export('Classifier');
 });
 
-Package.on_test(function(api) {
+Package.onTest(function(api) {
     api.use('flow-classifier');
 
     api.add_files('tests/flow-classifier_tests.js', ['client']);
